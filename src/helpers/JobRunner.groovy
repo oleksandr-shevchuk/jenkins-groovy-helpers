2fff5e39704e8f078650213cc65d8426767b9511
oleksandr-shevchuk <oleksandr.shv@gmail.com>
2022-04-14 12:42:36 +0300

    Update JobRunner.groovy

--- src/helpers/JobRunner.groovy
+++ src/helpers/JobRunner.groovy
@@ -1,24 +1,24 @@
 import hudson.console.ModelHyperlinkNote
 
-def context
-def currentBuild
+class JobRunner{
+    def context
+    def currentBuild
 
-def runJob(jobName, jobParameters){
-    println 'in lib'
-    def job = build job: jobName, parameters: jobParameters, propagate: false
-    context.println hudson.console.ModelHyperlinkNote.encodeTo("/job/${params.WORKER}/${j.number}/console", jobName + " #" + job.number) + " completed: " + job.result
-    if (job.result != "SUCCESS")
-    {
-        currentBuild.result = 'FAILURE'
+    def runJob(jobName, jobParameters){
+        println 'in lib'
+        def job = build job: jobName, parameters: jobParameters, propagate: false
+        context.println hudson.console.ModelHyperlinkNote.encodeTo("/job/${params.WORKER}/${j.number}/console", jobName + " #" + job.number) + " completed: " + job.result
+        if (job.result != "SUCCESS")
+        {
+            currentBuild.result = 'FAILURE'
+        }
+    }
+
+    def customLog(text){
+        println "custom log " + text
+    }
+
+    def testFunc(){
+        println "this is test func"
     }
 }
-
-def customLog(text){
-    println "custom log " + text
-}
-
-def testFunc(){
-    println "this is test func"
-}
-
-return this
