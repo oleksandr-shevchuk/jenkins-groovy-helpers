063d67c53795ca1ad6f39b4b9f35f9e24cba49af
oleksandr-shevchuk <oleksandr.shv@gmail.com>
2022-04-14 12:28:05 +0300

    Update JobRunner.groovy

--- src/helpers/JobRunner.groovy
+++ src/helpers/JobRunner.groovy
@@ -1,6 +1,7 @@
 import hudson.console.ModelHyperlinkNote
 
 def runJob(jobName, jobParameters){
+    println 'in lib'
     def job = build job: jobName, parameters: jobParameters, propagate: false
     context.println hudson.console.ModelHyperlinkNote.encodeTo("/job/${params.WORKER}/${j.number}/console", jobName + " #" + job.number) + " completed: " + job.result
     if (job.result != "SUCCESS")
