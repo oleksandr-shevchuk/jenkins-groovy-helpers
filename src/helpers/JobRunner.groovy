04c1d6c80163c3b88ac702436a29f09f1974c92a
oleksandr-shevchuk <oleksandr.shv@gmail.com>
2022-04-14 12:35:54 +0300

    Update JobRunner.groovy

--- src/helpers/JobRunner.groovy
+++ src/helpers/JobRunner.groovy
@@ -1,5 +1,7 @@
 import hudson.console.ModelHyperlinkNote
 
+package helpers
+
 def runJob(jobName, jobParameters){
     println 'in lib'
     def job = build job: jobName, parameters: jobParameters, propagate: false
